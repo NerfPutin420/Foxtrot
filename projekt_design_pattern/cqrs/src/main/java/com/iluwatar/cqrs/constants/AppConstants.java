2428b3255ac13277e981e7c9bdc5be033f7c6edb
Török Zoltán <h053273@inf.u-szeged.hu>
2022-11-23 21:01:53 +0100

    Cqrs major hiba javítása

--- projekt_design_pattern/cqrs/src/main/java/com/iluwatar/cqrs/constants/AppConstants.java
+++ projekt_design_pattern/cqrs/src/main/java/com/iluwatar/cqrs/constants/AppConstants.java
@@ -29,9 +29,14 @@ package com.iluwatar.cqrs.constants;
  */
 public class AppConstants {
 
+
+
   public static final String E_EVANS = "eEvans";
   public static final String J_BLOCH = "jBloch";
   public static final String M_FOWLER = "mFowler";
   public static final String USER_NAME = "username";
+
+  private AppConstants() {
+  }
 
 }
